733a35ff237759cf6bf951b081432151bf756cb7
Wai Yan Phyoe <waiyan4403@gmail.com>
2022-10-20 13:48:13 +0630

    Changed potrait to landscape

---
+++ Assets/UI Pack/Nightmare/Readme.txt
@@ -0,0 +1,86 @@
+------------------------------------------------------------------
+UI Pack : Nightmare 1.1.5
+------------------------------------------------------------------
+
+	Completed design for toony horror games.
+
+	Features:
+
+		• Backgrounds, particles, alphabets, characters, sounds and more.
+		• 336 prefabs.
+		• 29 sample layouts.
+		• 23 sample UI pages.
+		• 17 Sprites.
+		• 15 Particles.
+		• 4 sounds.
+		• 1 demo scene.
+		• Customisable and rearrangable.<br>
+		• HD textures.
+
+		• Support all build player platforms.
+		
+	Compatible:
+
+		• Unity 5.5.1 or higher.
+
+	Notes:
+	
+		1) All sprites and textures are PSD files.
+		2) Prefabs are Untiy UI and Unity Sprite.
+		3) Buttons, Banners, Gauges, Panel are color-changeable.
+		4) Canvases in the demo scene use 0.5 scale factor, so you can use 1.0 scale factor for UHD resolution.
+		5) Ground and hills in background are seamless tile-able.
+		6) Please feel free to use characters for commercial games.
+		7) Fonts are royalty free.
+		8) Music by Mike Koenig, Free-Loops.com (http://free-loops.com/7227-synthesizer-heaven.html).
+
+	Product page:
+	
+		https://www.assetstore.unity3d.com/en/#!/content/49146
+
+	Please direct any bugs/comments/suggestions to geteamdev@gmail.com
+		
+	Thank you for your support,
+
+	Gold Experience Team
+	E-mail: geteamdev@gmail.com
+	Website: https://www.ge-team.com
+
+------------------------------------------------------------------
+Release notes
+------------------------------------------------------------------
+
+	Version 1.1.5
+
+		• Unity 5.5.1 or higher compatible.
+
+	Version 1.1.0
+
+		• Unity 5.4.0 or higher compatible.
+
+	Version 1.0.6
+
+		• Unity 4.7.1 or higher compatible.
+		• Unity 5.3.4 or higher compatible.
+
+	Version 1.0.5
+
+		• Fixed GUID conflict with other packages.
+		• Unity 4.6.9 or higher compatible.
+		• Unity 5.3.2 or higher compatible.
+
+	Version 1.0 (Initial version)
+
+		• Complete design; background, particles, alphabets, character, sounds and more.
+		• Toony style, friendly for everyone.
+		• Customisable and rearrangable UIs.
+		• 336 Unity UI prefabs.
+		• 29 sample layouts.
+		• 23 pages of prefab collections
+		• 17 Sprites.
+		• 15 Particles.
+		• 4 sounds.
+		• 1 demo scene with sample C# scripts.
+		• HD textures.
+		• Supports all Unity player platforms.
+		• Compatible with multiple version of Unity; Unity 4.6.0 or higher, Unity 5.0.0 or higher and Unity 5.2.0 or higher.
---
+++ Assets/UI Pack/Nightmare/Scripts/DemoSceneController_Nightmare.cs
@@ -0,0 +1,312 @@
+﻿// UI Pack : Nightmare
+// Version: 1.1.5
+// Compatilble: Unity 5.5.1 or higher, see more info in Readme.txt file.
+//
+// Developer:			Gold Experience Team (https://www.assetstore.unity3d.com/en/#!/search/page=1/sortby=popularity/query=publisher:4162)
+//
+// Unity Asset Store:	https://www.assetstore.unity3d.com/en/#!/content/49146
+//
+// Please direct any bugs/comments/suggestions to geteamdev@gmail.com
+
+#region Namespaces
+using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine.UI;
+#endregion
+
+// ########################################
+// DemoSceneController_Nightmare class
+//
+// Show/hide objects and respond to user inputs.
+//
+// Note this sample script is attached with "-DemoSceneController-" object in "UI Pack/Scenes/Nightmare - Demo" scene.
+// ########################################
+
+public class DemoSceneController_Nightmare : MonoBehaviour
+{
+	// ########################################
+	// Variables
+	// ########################################
+
+	#region Variables
+
+	// Store Layouts/Prefabs types
+	public enum UIToggle
+	{
+		Layouts,
+		Prefabs
+	}
+
+	// Text
+	public Text m_Title;
+	public Text m_Order;
+
+	// Background
+	public GameObject m_Backgrounds = null;
+
+	// Layouts
+	public GameObject m_Layouts;
+	public Button m_LayoutButton;
+	public int m_LayoutIndex = 0;
+
+	// Prefabs
+	public GameObject m_Prefabs;
+	public Button m_PrefabButton;
+	public int m_PrefabIndex = 0;
+
+	// Type of displayed object.
+	public UIToggle m_UIToggle = UIToggle.Layouts;
+
+	GameObject m_ControlPanel = null;
+
+	#endregion // Variables
+
+	// ########################################
+	// MonoBehaviour Functions
+	// http://docs.unity3d.com/ScriptReference/MonoBehaviour.html
+	// ########################################
+
+	#region MonoBehaviour
+
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+	// http://docs.unity3d.com/ScriptReference/MonoBehaviour.Start.html
+	void Start()
+	{
+		UpdateLayoutAndPrefabButtons();
+		UpdateDisplay();
+	}
+
+	// Update is called every frame, if the MonoBehaviour is enabled.
+	// http://docs.unity3d.com/ScriptReference/MonoBehaviour.Update.html
+	void Update()
+	{
+
+#if UNITY_EDITOR || UNITY_STANDALONE
+		// Press C key to show/hide Control panel
+		if (Input.GetKeyUp(KeyCode.C))
+		{
+			if (m_ControlPanel == null)
+				m_ControlPanel = GameObject.Find("Control Panel");
+			if (m_ControlPanel)
+			{
+				if (m_ControlPanel.activeSelf)
+				{
+					m_ControlPanel.SetActive(false);
+					UpdateLayoutAndPrefabButtons();
+					UpdateDisplay();
+				}
+				else
+				{
+					m_ControlPanel.SetActive(true);
+				}
+			}
+		}
+		// Press Up/Down arrow keys to switch demo type
+		if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
+		{
+			OnToggleButton();
+			DemoSoundController_Nightmare.Instance.Play_SoundToggleDemoTypeButton();
+		}
+		// Press Left arrow key to show previous page
+		if (Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			OnPreviousButton();
+			DemoSoundController_Nightmare.Instance.Play_SoundNextPreviousButton();
+		}
+		// Press Right arrow key to show next page
+		if (Input.GetKeyUp(KeyCode.RightArrow))
+		{
+			OnNextButton();
+			DemoSoundController_Nightmare.Instance.Play_SoundNextPreviousButton();
+		}
+		// Press B key to show/hide Background
+		if (Input.GetKeyUp(KeyCode.B))
+		{
+			OnToggleBackground();
+		}
+		// Press M key to on/off Music
+		if (Input.GetKeyUp(KeyCode.M))
+		{
+			OnToggleBGMusic();
+		}
+#endif
+
+	}
+
+	#endregion // MonoBehaviour
+
+	// ########################################
+	// UI Responder functions
+	// ########################################
+
+	#region UI Responder
+
+	// User presses Previous Button
+	public void OnPreviousButton()
+	{
+		if (m_UIToggle == UIToggle.Layouts)
+		{
+			m_LayoutIndex--;
+			if (m_LayoutIndex < 0)
+				m_LayoutIndex = m_Layouts.transform.childCount - 1;
+
+			//Debug.Log("m_LayoutIndex=" + m_LayoutIndex);
+		}
+		else
+		{
+			m_PrefabIndex--;
+			if (m_PrefabIndex < 0)
+				m_PrefabIndex = m_Prefabs.transform.childCount - 1;
+
+			//Debug.Log("m_PrefabIndex=" + m_PrefabIndex);
+		}
+
+		ShowCurrentDisplayedObjects();
+	}
+
+	// User presses Next Button
+	public void OnNextButton()
+	{
+		if (m_UIToggle == UIToggle.Layouts)
+		{
+			m_LayoutIndex++;
+			if (m_LayoutIndex >= m_Layouts.transform.childCount)
+				m_LayoutIndex = 0;
+
+			//Debug.Log("m_LayoutIndex=" + m_LayoutIndex);
+		}
+		else
+		{
+			m_PrefabIndex++;
+			if (m_PrefabIndex >= m_Prefabs.transform.childCount)
+				m_PrefabIndex = 0;
+
+			//Debug.Log("m_PrefabIndex=" + m_PrefabIndex);
+		}
+
+		ShowCurrentDisplayedObjects();
+	}
+
+	// User presses Toggle display type button
+	public void OnToggleButton()
+	{
+		if (m_UIToggle == UIToggle.Layouts)
+		{
+			m_UIToggle = UIToggle.Prefabs;
+		}
+		else
+		{
+			m_UIToggle = UIToggle.Layouts;
+		}
+
+		UpdateLayoutAndPrefabButtons();
+		UpdateDisplay();
+	}
+
+	public void OnToggleBackground()
+	{
+		if (m_Backgrounds)
+		{
+			if (m_Backgrounds.activeSelf)
+			{
+				m_Backgrounds.SetActive(false);
+			}
+			else
+			{
+				m_Backgrounds.SetActive(true);
+			}
+		}
+	}
+
+	// User presses Toggle On/Off Music button
+	public void OnToggleBGMusic()
+	{
+		if (DemoSoundController_Nightmare.Instance.m_MusicIsOn == false)
+			DemoSoundController_Nightmare.Instance.SetMusic(true);
+		else
+			DemoSoundController_Nightmare.Instance.SetMusic(false);
+	}
+
+	// Toggle Layouts/Prefabs objects
+	void UpdateLayoutAndPrefabButtons()
+	{
+		if (m_UIToggle == UIToggle.Layouts)
+		{
+			m_LayoutButton.interactable = false;
+			m_LayoutButton.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+			m_PrefabButton.interactable = true;
+			m_PrefabButton.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
+			m_Title.color = new Color(0.3f, 0.6f, 1.0f, 1.0f);
+		}
+		else
+		{
+			m_LayoutButton.interactable = true;
+			m_LayoutButton.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
+			m_PrefabButton.interactable = false;
+			m_PrefabButton.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+			m_Title.color = new Color(0.6f, 1.0f, 0.6f, 1.0f);
+		}
+	}
+
+	// Show/Hide Layouts and Prefabs objects
+	void UpdateDisplay()
+	{
+		if (m_UIToggle == UIToggle.Layouts)
+		{
+			m_Layouts.SetActive(true);
+			m_Prefabs.SetActive(false);
+		}
+		else
+		{
+			m_Layouts.SetActive(false);
+			m_Prefabs.SetActive(true);
+		}
+		ShowCurrentDisplayedObjects();
+	}
+
+	#endregion // UI Responder
+
+	// ########################################
+	// Show/hide object functions
+	// ########################################
+
+	#region Show/hide object
+
+	// Show the children objects of Layouts/Prefabs object according to m_PrefabButton/m_LayoutButton.
+	void ShowCurrentDisplayedObjects()
+	{
+		int index = 0;
+		Transform trans;
+		if (m_UIToggle == UIToggle.Layouts)
+		{
+			index = m_LayoutIndex;
+			trans = m_Layouts.transform;
+		}
+		else
+		{
+			index = m_PrefabIndex;
+			trans = m_Prefabs.transform;
+		}
+
+		int i = 0;
+		foreach (Transform child in trans)
+		{
+			if (i == index)
+			{
+				child.gameObject.SetActive(true);
+				m_Title.text = child.name;
+				m_Order.text = (index + 1).ToString() + "/" + trans.childCount.ToString();
+			}
+			else
+			{
+				child.gameObject.SetActive(false);
+			}
+
+			i++;
+		}
+	}
+
+	#endregion // Show/hide object
+}
